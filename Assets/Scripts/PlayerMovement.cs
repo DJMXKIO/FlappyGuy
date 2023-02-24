c1eccbcb3f909aa7d21eaab2081921c6183fe0c6
Jennifyy <JennaStillings@yahoo.com>
2023-02-24 13:10:47 -0800

    ANOTHER UPDATE WOW
    crying

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class PlayerMovement : MonoBehaviour
 {
     
     private Rigidbody2D rb;
-    
+    private float jumpForce = 600f;
+
+    [SerializeField] private AudioSource jumpSoundEffect;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +24,20 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey("space"))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector3(0, 5f, 0);
+            jumpSoundEffect.Play();
+        }
+        if (Input.touchCount > 0 && Input.GetTouch(8).phase == TouchPhase.Began)
+        {
+           // jumpSoundEffect.Play();
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Force);
         }
     }
-   
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        LoadScene("Menu");
+    }
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 }
